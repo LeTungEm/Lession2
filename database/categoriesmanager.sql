44802a4fa0d5df5fb6e1fdceb7ebfce13afa1a66
LeTungEm <em.le.tung2103@gmail.com>
2023-03-18 14:51:56 +0700

    fix edit function

---
+++ database/categoriesmanager.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th3 17, 2023 lúc 06:30 PM
+-- Phiên bản máy phục vụ: 10.4.19-MariaDB
+-- Phiên bản PHP: 7.4.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `categoriesmanager`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `parent_id` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `parent_id`) VALUES
+(1, 'Category 1', 0),
+(2, 'Category 11', 1),
+(3, 'Category 111', 2),
+(4, 'Category 12', 1),
+(5, 'Category 2', 0),
+(6, 'Category 21', 5),
+(7, 'Category 22', 5),
+(8, 'Category 3', 0),
+(9, 'Category 4', 0);
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/Controllers/CategoryController/index.php
+++ src/Controllers/CategoryController/index.php
@@ -4,28 +4,24 @@ $categoryList = $category->getAll();
 $categoryTree = array();
 $categoryTree[] = $category->buildTree('0', $categoryList);
 
-if (isset($_POST["btnInsertCategory"])) 
-{
+if (isset($_POST["btnInsertCategory"])) {
     $categoryName = $_POST["categoryName"];
     $parentCategory = $_POST['parentCategory'];
     print_r($category->insertCategory($categoryName, $parentCategory));
 }
 
-if (isset($_POST["btnDeleteCategory"])) 
-{
+if (isset($_POST["btnDeleteCategory"])) {
     $deleteId = $_POST["deleteId"];
     function deleteCategory($list, $deleteId)
     {
         foreach ($list as $value) {
             // xóa các phần tử con của phần tử cần xóa
-            if($value->getParentId() == $deleteId)
-            {
+            if ($value->getParentId() == $deleteId) {
                 $value->deleteCategory($value->getId());
-                deleteCategory($value->getListChild()[0], $value->getId());     
+                deleteCategory($value->getListChild()[0], $value->getId());
             }
             // Duyệt categoryTree
-            else if (count($value->getListChild())) 
-            {
+            else if (count($value->getListChild())) {
                 deleteCategory($value->getListChild()[0], $deleteId);
             }
         }
@@ -33,40 +29,49 @@ if (isset($_POST["btnDeleteCategory"]))
     deleteCategory($categoryTree[0], $deleteId);
     // Xóa phần tử có id đã chọn
     $category->deleteCategory($deleteId);
-    
+
 }
 
-if(isset($_POST["btnEditCategory"]))
-{
+if (isset($_POST["btnEditCategory"])) {
     $categoryId = $_POST["editCategoryId"];
     $categoryName = $_POST["editCategoryName"];
     $parentCategory = $_POST['editParentCategory'];
-    if($categoryId == $parentCategory)
+
+    function findCategory($list, $parentId, $changeParentId)
     {
-        echo "loi";
-    }else{
+        foreach ($list as $value) {
+            // tìm các phần tử con của phần tử cần copy
+            if ($value->getParentId() == $parentId) {
+                if ($value->getId() == $changeParentId) {
+                    return false;
+                }
+                if(!findCategory($value->getListChild()[0], $value->getId(), $changeParentId)){
+                    return false;
+                }
+            }
+            // Duyệt categoryTree
+            else if (count($value->getListChild())) {
+                if(!findCategory($value->getListChild()[0], $parentId, $changeParentId)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    if ($categoryId == $parentCategory) {
+
+    } else if (!findCategory($categoryTree[0], $categoryId, $parentCategory)) {
+        echo 'Trung phan tu con';
+    } else {
         $category->editCategory($categoryName, $parentCategory, $categoryId);
     }
 
-    // function findInChildCategory($categoryList, $id, $parentId)
-    // {
-    //     foreach ($categoryList as $value)
-    //     {
-    //         if($value->getParentId() == $id){
-    //             if($value->getId() == $parentId)
-    //                 echo "co";
-    //         }
-    //         else
-    //         {
-    //             findInChildCategory($value->getListChild()[0], $id, $parentId);
-    //         }
-    //     }
-    // }
-    // findInChildCategory($categoryTree[0], $categoryId, $parentCategory);
+
+
 }
 
-if(isset($_POST["btnCopyCategory"]))
-{
+if (isset($_POST["btnCopyCategory"])) {
     $copyCategoryId = $_POST['copyCategoryId'];
     $copyCategoryName = $_POST["copyCategoryName"];
     $copyParentCategory = $_POST["copyParentCategory"];
@@ -75,14 +80,12 @@ if(isset($_POST["btnCopyCategory"]))
     {
         foreach ($list as $value) {
             // copy các phần tử con của phần tử cần copy
-            if($value->getParentId() == $parentId)
-            {
+            if ($value->getParentId() == $parentId) {
                 $newParentId = $value->insertCategory($value->getName(), $newId);
-                copyCategory($value->getListChild()[0], $value->getId(), $newParentId);     
+                copyCategory($value->getListChild()[0], $value->getId(), $newParentId);
             }
             // Duyệt categoryTree
-            else if (count($value->getListChild())) 
-            {
+            else if (count($value->getListChild())) {
                 copyCategory($value->getListChild()[0], $parentId, $newId);
             }
         }
